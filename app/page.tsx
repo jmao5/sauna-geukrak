7ce1eb33922bcc7fbd143a3cb32623fe6eea273b
전상현_FO-FE_소프트웍스 <jsh373805@softworks.co.kr>
2026-04-29 19:26:35 +0900

    Revert "지도ssr -> csr 수정"
    This reverts commit 9a5a70d07086bdb369a55a4fc39b4730a5680083.

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import HomeClient from './HomeClient'
 export default async function HomePage() {
   const queryClient = new QueryClient()
 
-  queryClient.prefetchQuery({
+  await queryClient.prefetchQuery({
     queryKey: ['saunas'],
     queryFn: async () => {
       const supabase = await createClient()
